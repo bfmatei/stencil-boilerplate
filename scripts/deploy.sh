d5ec935f2e97338d772b20ae2c6c82a285854616
Bogdan Matei <contact@bmatei.com>
2018-01-22 08:40:58 +0200

    add removal of www folder and reset the repo before build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 ssh -o "StrictHostKeyChecking no" ${HOST_USERNAME:-hostUsername}@${HOST:-host} "
   cd ~/projects/stencil-boilerplate;
+  rm -rf www;
+  git reset --hard HEAD;
   git pull;
   npm install;
   npm run build;
