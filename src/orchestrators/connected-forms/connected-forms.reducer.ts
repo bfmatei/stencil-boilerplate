a2d904d8c14f29ea7ead5567383d2d6d56aa334d
Bogdan Matei <contact@bmatei.com>
2018-02-04 11:59:08 +0200

    Refactor connected form reducer

--- src/orchestrators/connected-forms/connected-forms.reducer.ts
+++ src/orchestrators/connected-forms/connected-forms.reducer.ts
@@ -20,56 +20,75 @@ export function getInitialState(): ConnectedFormsState {
   return {};
 }
 
+function convertArrayToObject(arr: any[], key: string): {} {
+  return arr.reduce((acc: {}, item: {}[]) => {
+    return {
+      ...acc,
+      [item[key]]: {
+        ...item
+      }
+    };
+  }, {});
+}
+
+function computeErrorFlag(fields: ConnectedFormFields): boolean {
+  return !Object.keys(fields)
+    .every((field: string): boolean => {
+      return fields[field].error === false;
+    });
+}
+
 export default function forms(state: ConnectedFormsState = getInitialState(), action: ConnectedFormsActionTypes): ConnectedFormsState {
+  let currentFormName: string = '';
   let currentForm: ConnectedForm = null;
+  let currentFormFields: ConnectedFormFields = {};
+  let currentFieldName: string = '';
+  let currentField: ConnectedFormField = null;
 
   switch (action.type) {
     case ConnectedFormsActions.REGISTER_FORM:
+      currentFormName = action.payload.name;
+      currentFormFields = convertArrayToObject(action.payload.fields, 'name');
+
       return {
         ...state,
-        [action.payload.name]: {
-          name: action.payload.name,
+        [currentFormName]: {
+          name: currentFormName,
           success: false,
-          error: !action.payload.fields.every((field: ConnectedFormField): boolean => {
-            return field.error === false;
-          }),
+          error: computeErrorFlag(currentFormFields),
           submitting: false,
           dirty: false,
-          fields: action.payload.fields.reduce((acc: ConnectedFormFields, item: ConnectedFormField): ConnectedFormFields => {
-            return {
-              ...acc,
-              [item.name]: {
-                ...item,
-                disabled: false
-              }
-            };
-          }, {})
+          fields: currentFormFields
         }
       };
 
     case ConnectedFormsActions.UNREGISTER_FORM:
+      currentFormName = action.payload;
+
       return {
         ...state,
-        [action.payload]: undefined
+        [currentFormName]: undefined
       };
 
     case ConnectedFormsActions.SUBMIT_FORM:
-      currentForm = state[action.payload];
+      currentFormName = action.payload;
+      currentForm = state[currentFormName];
+      currentFormFields = currentForm.fields;
 
       return {
         ...state,
-        [action.payload]: {
+        [currentFormName]: {
           ...currentForm,
           success: false,
           error: false,
           submitting: true,
           fields: {
-            ...Object.keys(currentForm.fields)
+            ...Object.keys(currentFormFields)
               .reduce((acc: ConnectedFormFields, field: string) => {
                 return {
                   ...acc,
                   [field]: {
-                    ...currentForm.fields[field],
+                    ...currentFormFields[field],
                     error: false,
                     message: ''
                   }
@@ -80,11 +99,12 @@ export default function forms(state: ConnectedFormsState = getInitialState(), ac
       };
 
     case ConnectedFormsActions.SUBMIT_FORM_SUCCESS:
-      currentForm = state[action.payload];
+      currentFormName = action.payload;
+      currentForm = state[currentFormName];
 
       return {
         ...state,
-        [action.payload]: {
+        [currentFormName]: {
           ...currentForm,
           success: true,
           submitting: false
@@ -92,21 +112,25 @@ export default function forms(state: ConnectedFormsState = getInitialState(), ac
       };
 
     case ConnectedFormsActions.SUBMIT_FORM_ERROR:
-      currentForm = state[action.payload.name];
+      currentFormName = action.payload.name;
+      currentForm = state[currentFormName];
+      currentFormFields = currentForm.fields;
 
       return {
         ...state,
-        [action.payload.name]: {
+        [currentFormName]: {
           ...currentForm,
           error: true,
           submitting: false,
           fields: {
-            ...currentForm.fields,
+            ...currentFormFields,
             ...action.payload.errors.reduce((acc: ConnectedFormFields, currentError: AppFormError) => {
+              currentFieldName = currentError.field;
+
               return {
                 ...acc,
-                [currentError.field]: {
-                  ...currentForm.fields[currentError.field],
+                [currentFieldName]: {
+                  ...currentFormFields[currentFieldName],
                   error: true,
                   message: currentError.message
                 }
@@ -117,15 +141,18 @@ export default function forms(state: ConnectedFormsState = getInitialState(), ac
       };
 
     case ConnectedFormsActions.REGISTER_FIELD:
-      currentForm = state[action.payload.formName];
+      currentFormName = action.payload.formName;
+      currentForm = state[currentFormName];
+      currentFormFields = currentForm.fields;
+      currentFieldName = action.payload.field.name;
 
       return {
         ...state,
-        [action.payload.formName]: {
+        [currentFormName]: {
           ...currentForm,
           fields: {
-            ...currentForm.fields,
-            [action.payload.field.name]: {
+            ...currentFormFields,
+            [currentFieldName]: {
               ...action.payload.field
             }
           }
@@ -133,37 +160,47 @@ export default function forms(state: ConnectedFormsState = getInitialState(), ac
       };
 
     case ConnectedFormsActions.SET_FIELD_VALUE:
-      currentForm = state[action.payload.formName];
+      currentFormName = action.payload.formName;
+      currentForm = state[currentFormName];
+      currentFormFields = currentForm.fields;
+      currentFieldName = action.payload.name;
+      currentField = currentFormFields[currentFieldName];
+
+      currentFormFields = {
+        ...currentFormFields,
+        [currentFieldName]: {
+          ...currentField,
+          error: !!action.payload.err,
+          message: action.payload.err || currentField.message,
+          value: action.payload.value
+        }
+      };
 
       return {
         ...state,
-        [action.payload.formName]: {
+        [currentFormName]: {
           ...currentForm,
           dirty: true,
-          error: currentForm.error || !!action.payload.err,
-          fields: {
-            ...currentForm.fields,
-            [action.payload.name]: {
-              ...currentForm.fields[action.payload.name],
-              error: !!action.payload.err,
-              message: action.payload.err || currentForm.fields[action.payload.name].message,
-              value: action.payload.value
-            }
-          }
+          error: computeErrorFlag(currentFormFields),
+          fields: currentFormFields
         }
       };
 
     case ConnectedFormsActions.SET_FIELD_PROP:
-      currentForm = state[action.payload.formName];
+      currentFormName = action.payload.formName;
+      currentForm = state[currentFormName];
+      currentFormFields = currentForm.fields;
+      currentFieldName = action.payload.name;
+      currentField = currentFormFields[currentFieldName];
 
       return {
         ...state,
-        [action.payload.formName]: {
+        [currentFormName]: {
           ...currentForm,
           fields: {
-            ...currentForm.fields,
-            [action.payload.name]: {
-              ...currentForm.fields[action.payload.name],
+            ...currentFormFields,
+            [currentFieldName]: {
+              ...currentField,
               [action.payload.prop]: action.payload.value
             }
           }
